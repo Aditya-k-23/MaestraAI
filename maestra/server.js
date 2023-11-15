df5137e9d0b05aa038c12df0959eed47da5b1c52
Jacob Matias <matias.jacob2505@gmail.com>
2023-11-15 10:56:17 -0500

    added some changes for integration of web2lip in nodejs

--- maestra/server.js
+++ maestra/server.js
@@ -31,5 +31,45 @@ app.post('/completions', async (req, res) => {
         console.error(error)
     }
 })
+
+app.post('/wav2lip', (req, res) => {
+    const spawn = require('child_process').spawn
+    const scriptPath = "./wav2lip/generate.py" 
+    const videoFile = "test5.mp4"
+    const audioFile = "Recording.mp3"
+    const pyProcess = spawn('python', [scriptPath, videoFile, audioFile])
+    
+    pyProcess.stdout.on('data', (data) => {
+        console.log(data.toString())
+        res.write(data)
+        res.end('end')
+    })
+
+
+})
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: "how are you?" }],
+            max_tokens: 250,
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+
 app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
 
--- maestra/src/App.jsx
+++ maestra/src/App.jsx
@@ -4,6 +4,17 @@ import './App.css';
 
 const App = () => {
 
+  const generateLipsyncVideo = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/wav2lip', { method: "POST" })
+      const data = await response.json()
+
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   const getMessages = async () => {
     const options = {
       method: "POST",
@@ -27,6 +38,10 @@ const App = () => {
     <div className="App">
       <section className="side-bar">
         <button onClick={getMessages}>+ New Chat </button>
+        <button onClick={generateLipsyncVideo} id="test-wav2lip-btn">generate video with Wav2Lip</button>
+        <video controls width="250">
+          <source src="/media/result_voice.mp4" type="video/mp4" />
+        </video>
         <nav>
           <p>MaestraAI</p>
         </nav>
