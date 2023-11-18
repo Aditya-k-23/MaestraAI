8cf1740a4660c7433ee2e432a7b6e7688f5a575e
rkwan05 <rkwan.hy@gmail.com>
2023-11-18 15:46:27 -0500

    eleven api setup and chat ai integration with front end

--- maestra/server.js
+++ maestra/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 
-const API_KEY = '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'
+const API_KEY = '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'
 
 app.post('/completions', async (req, res) => {
     const options = {
@@ -19,8 +19,8 @@ app.post('/completions', async (req, res) => {
         },
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: "how are you?" }],
-            max_tokens: 250,
+            messages: [{ role: "user", content: req.body.message }],
+            max_tokens: 100,
         })
     }
     try {
--- maestra/src/App.js
+++ maestra/src/App.js
@@ -1,37 +1,41 @@
 import './App.css';
+import AudioComponent from './audio';
+import ChatComponent from './chat';
 // import fetch from "node-fetch";
 
 
 const App = () => {
 
-  const getMessages = async () => {
-    const options = {
-      method: "POST",
-      body: JSON.stringify({
-        message: "how are you?"
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-    try {
-      const response = await fetch('http://localhost:8000/completions', options)
-      const data = await response.json()
-      console.log(data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  // const getMessages = async () => {
+  //   const options = {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       message: "how are you?"
+  //     }),
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   }
+  //   try {
+  //     const response = await fetch('http://localhost:8000/completions', options)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 
   return (
     <div className="App">
       <section className="side-bar">
-        <button onClick={getMessages}>+ New Chat </button>
+        <button>+ New Chat </button>
         <nav>
           <p>MaestraAI</p>
         </nav>
       </section>
       <section className="main"></section>
+      <AudioComponent></AudioComponent>
+      <ChatComponent></ChatComponent>
     </div>
   );
 }
